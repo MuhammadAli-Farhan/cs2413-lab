0e3ad9d5b4cb5437ccbd1ad7f846a46eae9bc00e
Muhammad Ali Farhan <mufarhan@ttu.edu>
2026-02-19 17:17:00 -0600

    Finish Assignment1 (Q1, Q2, Q3)

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -1,22 +1,20 @@
 #include "Student.h"
 
-//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n:
-//nums1 has length m + n
-//The first m values in nums1 are real elements
-//The last n values in nums1 are placeholders (0)
-//nums2 has length n
-//Goal: Merge nums2 into nums1 so that nums1 becomes one sorted array.
-//Return: Nothing (modify nums1 in-place).
-
-//Example:
-//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-//Output: nums1 = [1,2,2,3,5,6]
-//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int total = m + n;
 
- // TODO: implement
+    if (n > nums2Size) n = nums2Size;
+    if (total > nums1Size) total = nums1Size;
 
+    int i = m - 1;
+    int j = n - 1;
+    int k = total - 1;
 
-
+    while (j >= 0 && k >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
 }
