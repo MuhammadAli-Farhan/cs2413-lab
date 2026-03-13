e8b994685322e2bcb91260b3d9de0046cd4b65ba
Muhammad Ali Farhan <mufarhan@ttu.edu>
2026-03-13 12:48:01 -0500

    Done

--- lab04/Question2/student.c
+++ lab04/Question2/student.c
@@ -1,47 +1,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-/*
-Question 2: Symmetric Tree
-
-Given the root of a binary tree, check whether it is symmetric
-around its center.
-
-A tree is symmetric if the left subtree is a mirror reflection
-of the right subtree.
-
-Examples:
-
-Example 1:
-Input:
-        1
-       / \
-      2   2
-     / \ / \
-    3  4 4  3
-Output: true
-
-Example 2:
-Input:
-        1
-       / \
-      2   2
-       \   \
-       3    3
-Output: false
-
-Function to complete:
-    bool isSymmetric(struct TreeNode* root);
-
-Note:
-- An empty tree is considered symmetric.
-- You should compare the left subtree and right subtree
-  as mirror images.
-*/
-
-/*
- * Definition for a binary tree node.
- */
 struct TreeNode {
     int val;
     struct TreeNode *left;
@@ -51,5 +10,36 @@ struct TreeNode {
 
 
 bool isSymmetric(struct TreeNode* root) {
-  // TODO: implement
+
+    if (root == NULL)
+        return true;
+
+    struct TreeNode* queue[1000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root->left;
+    queue[rear++] = root->right;
+
+    while (front < rear) {
+
+        struct TreeNode* left = queue[front++];
+        struct TreeNode* right = queue[front++];
+
+        if (left == NULL && right == NULL)
+            continue;
+
+        if (left == NULL || right == NULL)
+            return false;
+
+        if (left->val != right->val)
+            return false;
+
+        queue[rear++] = left->left;
+        queue[rear++] = right->right;
+
+        queue[rear++] = left->right;
+        queue[rear++] = right->left;
+    }
+
+    return true;
 }
