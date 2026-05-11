3ba066b47319fef8d80eff9266de3e033911a57d
Muhammad Ali Farhan <mufarhan@ttu.edu>
2026-05-11 15:14:19 -0500

    Complete lab07

--- lab07/Question7/student.c
+++ lab07/Question7/student.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int arr[], int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d", arr[i]);
+
+        if (i < size - 1) {
+            printf(" ");
+        }
+    }
+    printf("]\n");
+}
+
+int isSorted(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        if (arr[i - 1] > arr[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void insertionSort(int arr[], int left, int right) {
     for (int i = left + 1; i <= right; i++) {
         int key = arr[i];
@@ -74,4 +98,27 @@ void hybridMergeSort(int arr[], int left, int right, int k) {
     hybridMergeSort(arr, mid + 1, right, k);
 
     merge(arr, left, mid, right);
+}
+
+int main() {
+    int arr[] = {39, 9, 81, 45, 90, 27, 72, 18};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int k = 3;
+
+    printf("Original array:\n");
+    printArray(arr, size);
+
+    hybridMergeSort(arr, 0, size - 1, k);
+
+    printf("Sorted array:\n");
+    printArray(arr, size);
+
+    if (isSorted(arr, size)) {
+        printf("Result: PASS\n");
+    } else {
+        printf("Result: FAIL\n");
+    }
+
+    return 0;
 }
